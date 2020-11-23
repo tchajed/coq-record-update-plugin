7c4f9b4b4a645c9bbfca6cdc18bfe8a0e61b2c12
Tej Chajed <tchajed@mit.edu>
2020-11-23 17:43:42 -0600

    Clean up some plugin code

--- src/eta_expansion.ml
+++ src/eta_expansion.ml
@@ -6,14 +6,11 @@ let solve_with_eta (r: EConstr.t) =
   let env = Global.env () in
   let sigma = Evd.from_env env in
   match EConstr.kind sigma r with
-  | Constr.Ind ((i, i_index), u) ->
-    let ((i, i_index), u) = EConstr.destInd sigma r in
-
+  | Constr.Ind (ind, u) ->
     (* TODO: check if r has multiple constructors (can't be treated as a record) -
        0 is the constructor index here *)
-    let r_constructor = EConstr.mkConstructU (((i, i_index), 0 + 1), u) in
-
-    let accessor_opts = Recordops.lookup_projections (i, i_index) in
+    let r_constructor = EConstr.mkConstructU ((ind, 0 + 1), u) in
+    let accessor_opts = Recordops.lookup_projections ind in
     let accessor_constrs = List.map (fun a_o -> EConstr.mkConst (Option.get a_o)) accessor_opts in
     let body = List.fold_left
         (fun acc proj ->
