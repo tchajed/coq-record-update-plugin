cb1559afba2eaab5599a889778ef9d79be1d2e9f
Tej Chajed <tchajed@mit.edu>
2020-11-23 17:01:15 -0600

    Switch to EConstr
    Fixed a bug where the Setter tactic wasn't working as just a hint, it
    had to be called for an instance first (got an anomaly about grounding a
    non-evar-free term or something along those lines).

--- theories/Demo.v
+++ theories/Demo.v
@@ -1,7 +1,4 @@
 From RecordUpdate Require Import RecordUpdatePlugin.
 
 Record X := { A : nat; B : nat; C : bool; }.
-
-Instance etaX : Settable X := _.
-
 Definition setA a x := set A (fun _ => a) x.
